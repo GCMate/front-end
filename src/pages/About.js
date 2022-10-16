05b79bf88a11c51a8107827f900189adf19e9174
BianoBonzo <brianlin16@hotmail.com>
2022-10-16 12:44:48 -0400

    Implement RIN Input front page

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import gcmateLogo from '../img/GCMateIcon.png';
+
+const About = () => {
+    return (
+        <div className="About" style={{
+            position: 'absolute',
+            backgroundImage: 'url('+gcmateLogo+')',
+            backgroundSize: "cover",
+            height: "721px",
+            width: "1535px" 
+    }}>
+
+    <h1> Test </h1>
+    </div>
+    );
+};
+
+export default About; 
