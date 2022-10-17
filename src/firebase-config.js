aa08caddfc5d8d10c1ed6fddf1f9e4edde2b42ec
BianoBonzo <brianlin16@hotmail.com>
2022-10-17 15:58:43 -0400

    Implement phone number verification page

---
+++ src/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDiP9EkprUpfhyBBqdpTwmOItdPVuFNgj8",
+    authDomain: "phone-auth-5eeeb.firebaseapp.com",
+    projectId: "phone-auth-5eeeb",
+    storageBucket: "phone-auth-5eeeb.appspot.com",
+    messagingSenderId: "78836410767",
+    appId: "1:78836410767:web:dd9239540208233c9403fa"
+};
+
+const app = initializeApp(firebaseConfig);
+export const authentication = getAuth(app);
